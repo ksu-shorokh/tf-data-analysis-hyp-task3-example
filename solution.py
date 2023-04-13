13a5c8f868534f70b0e3c8ae49c130434f3e1fbb
ksu-shorokh <91955471+ksu-shorokh@users.noreply.github.com>
2023-04-14 00:44:53 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 
 chat_id = 845786312 # Ваш chat ID, не меняйте название переменной
+
 
 def solution(x:np.array, y:np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    res = ttest_ind(x, y)
-    return res[1] < 0.03
+    res = mannwhitneyu(x, y).pvalue
+    return res < 0.03
